97a58866d9f308bf1654f853cb36ce7f3fa0c471
Massimo Lauria <massimo.lauria@uniroma1.it>
2022-12-01 16:12:56 +0100

    AOC day 01

--- 2022/code01.py
+++ 2022/code01.py
@@ -1,6 +1,3 @@
-
-
-
 text="""
 1000
 2000
@@ -40,8 +37,31 @@ def aoc1part1(data=None):
     s=[]
     for x in get_numbers(data):
         s.append(sum(x))
-    print(max(s))
+    return max(s)
 
+def aoc1part2(data=None):
+    v = 0
+    s = [-1,-1,-1]
+    for l in parsedata(data)+['']:
+        if len(l) != 0:
+            v += int(l)
+            continue
+        if v==0:
+            continue
+        if v > s[0]:
+            s.insert(0,v)
+        elif v > s[1]:
+            s.insert(1,v)
+        elif v > s[2]:
+            s.insert(2,v)
+        else:
+            s.append(v)
+        s.pop()
+        v = 0
+    return sum(s)
 
 if __name__ == "__main__":
-    aoc1part1()
+    print(aoc1part1(text))
+    print(aoc1part1())
+    print(aoc1part2(text))
+    print(aoc1part2())
