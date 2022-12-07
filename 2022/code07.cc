5cb6075cd85cdf1d8866d9ca5ffd0ab5ade5f810
Massimo Lauria <massimo.lauria@uniroma1.it>
2022-12-07 11:01:30 +0100

    Day 07

--- 2022/code07.cc
+++ 2022/code07.cc
@@ -5,8 +5,8 @@
 #include <sstream>
 #include <fstream>
 
-//#include <vector>
-//#include <string>
+#include <vector>
+#include <string>
 //#include <unordered_set>
 //#include <unordered_map>
 
@@ -14,10 +14,49 @@ using namespace std;
 
 // Data
 string EXAMPLE {R"~(
+$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k
 )~"};
 
 string INPUTFILE="input07.txt";
 
+struct fsnode {
+    bool isfile=false;
+    int  size=0;
+    string name;
+    vector<fsnode*> children;
+};
+
+fsnode&& createfs(istream &input) {
+    string line;
+    for(int i=0; i<4;i++) {
+        getline(input,line);
+        if (line.size()==0) continue;
+        cout<<line<<endl;
+    }
+    return {false,0,"/", vector<fsnode*> {} };
+}
 
 void part1(istream& input) {
     cout<<"part1"<<endl;
@@ -33,7 +72,7 @@ int main () {
     // part1 - example data
     istringstream example {EXAMPLE} ;
     part1(example);
-
+    createfs(example);
     // part2 - input file
     ifstream  input {INPUTFILE};
     if (!input) {
