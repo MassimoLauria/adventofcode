d68c45b991e1e6577eeeb99b139e57b81bcfbe91
Massimo Lauria <massimo.lauria@uniroma1.it>
2022-12-16 16:41:09 +0100

    AOC day 16 - part 1

--- 2022/code16.py
+++ 2022/code16.py
@@ -15,7 +15,8 @@ Valve JJ has flow rate=21; tunnel leads to valve II
 """
 
 import re
-from collections import defaultdict
+import math
+from collections import defaultdict, deque
 
 def readdata(data=None):
     """Read and parse the input data"""
@@ -32,45 +33,87 @@ def readdata(data=None):
         P[linfo[0]] = int(linfo[1])
     return G,P
 
+def BFS(G,v):
+    Q=deque()
+    dist={ w:math.inf for w in G}
+    dist[v]=0
+    Q.append(v)
+    while len(Q):
+        x = Q.popleft()
+        for y in G[x]:
+            if dist[y]==math.inf:
+                Q.append(y)
+                dist[y] = dist[x]+1
+    return dist
+
+def process_graph(G,useful_nodes):
+    D = {}
+    for v in useful_nodes:
+        dist = BFS(G,v)
+        D[v] = { w: dist[w]+1 for w in useful_nodes if v!=w }
+    return D
 
 def part1(data=None):
     """solve part 1"""
+
     graph, pressures = readdata(data)
-    print(graph)
-    print(pressures)
+    useful_nodes = set([w for w in graph if pressures[w]>0])
+    useful_nodes.add('AA')
+
+    D = process_graph(graph,
+                      useful_nodes)
+
+    # never return to 'AA'
+    if pressures['AA']==0:
+        for v in D:
+            if 'AA' in D[v]:
+                D[v].pop('AA')
+
     best_value=0
+    best_path=0
     curr_value=0
     available = sum(pressures.values())
-    Open = set()
-    path = ['AA']
-    def branch():
-        nonlocal best_value,path,curr_value,Open,graph
-        pos = path[-1]
-        rtime = 31-len(path)-len(Open)
-        if rtime<=0:
-            return curr_value
-        # do not open
-        for v in graph[pos]:
-            path.append(v)
-            best_value = max(best_value,branch())
-            path.pop()
-        # open (if possible)
-        if pressures[pos]==0 or pos in Open:
-            return best_value
+    endtime=30
+    curr_path = ['AA']
 
-        Open.add(pos)
-        curr_value += pressures[pos]*(rtime-1)
+    def branch(time):
 
-        for v in graph[pos]:
-            path.append(v)
-            best_value = max(best_value,branch())
-            path.pop()
-        Open.remove(pos)
-        return best_value
-    print(branch())
+        nonlocal D
+        nonlocal best_value,best_path
+        nonlocal curr_value,curr_path
+        nonlocal available
+
+        pos = curr_path[-1]
+        #print(curr_value,curr_path,"against",best_value)
+        #if curr_value>best_value:
+        #    best_value,best_path = curr_value,curr_path[:]
+        best_value = max(curr_value,best_value)
+
+        # cut paths with no advantage
+        if curr_value+available*(time-1) <= best_value:
+            return
+
+        # choose next to open
+        for npos in D[pos]:
+
+            d = D[pos][npos]
+
+            if npos in curr_path or d>time:
+                continue
+
+            curr_path.append(npos)
+            curr_value += pressures[npos]*(time-d)
+            available  -= pressures[npos]
+            branch(time-d)
+            curr_path.pop()
+            curr_value -= pressures[npos]*(time-d)
+            available  += pressures[npos]
+
+    branch(endtime)
+    print("part1:", best_value)
 
 
 if __name__ == "__main__":
     part1(EXAMPLE)
-    # part1()
+    part1()
     # part2()
