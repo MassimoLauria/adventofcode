ab173b4c62ffe7a12ced260755d2436f91408b49
Massimo Lauria <massimo.lauria@uniroma1.it>
2022-12-18 23:30:23 +0100

    AOC 17

--- 2022/code17.py
+++ 2022/code17.py
@@ -59,48 +59,112 @@ def printchamber(chamber,piece=None,pos=-1):
     while i>=0:
         x="{:07b}".format(output[i]).translate(tr)
         print('{}{}{}'.format(i%10,x,i%10),end="")
-        if output[i] == 127 and i!=0: print("<<<<<<<<",end="")
-        print("  ",chr(output[i])," ")
+        print("  ",repr(chr(output[i]))," ",end="")
+        if output[i] == 127 and i!=0: print("<<<<<<<<")
+        print()
         i-=1
 
-def part1(duration,data=None):
-    """solve part 1"""
+def height(pit):
+    # Find tallest element
+    h=len(pit)-1
+    while pit[h] == 0x00:
+        h -= 1
+    return h
+
+def next_state(p,j,pit,jets):
+    """simulate a single step
+
+    returns a pair (p,j,pit)
+    p next piece
+    j next jet
+    pit status
+    """
+    assert 0<=p<len(pieces)
+    assert 0<=j<len(jets)
+
+    # Find tallest element
+    pos   = height(pit) + 4
+    pit   = bytearray(pit)
+    piece = pieces[p][:]
+    while True:
+
+        tmp = tryleft(piece) if jets[j] =='<' else tryright(piece)
+        j = ( j + 1 ) % len(jets)
+
+        if not collision(pit,tmp,pos):
+            piece = tmp
+        if not collision(pit,piece,pos-1):
+            pos -= 1
+        else:
+            break
+    place(pit,piece,pos)
+    return (p+1) % 5 , j, bytes(pit)
+
+def solve_naive(data,duration):
+    "Naive simulation"
     jets = readdata(data)
-    #print(len(jets))
-    #print(len(pieces))
-    pit = bytearray([127])
-    pos = 0    # piece position (from bottom)
-    highest=0  # position of highest block
+    pit = bytes([127])
     j = 0
+    p = 0
+    for _ in range(duration):
+        p,j,pit = next_state(p,j,pit,jets)
+    return height(pit)
 
-    for i in range(duration):
-        piece = pieces[i % len(pieces)][:]
-        pos   = highest + 4
-        while True:
+def solve_cycle_detect(data,duration):
+    "Simulation with cycle detection"
+    jets = readdata(data)
+    MEM = 10
 
-            jet = jets[j]
-            j = (j + 1) % len(jets)
+    Confs  = set() # visited configurations
+    T      = {}    # time of first visit
+    H      = {}    # height at time of first visit
 
-            if jet=='<':
-                tmp = tryleft(piece)
-            else:
-                assert jet=='>'
-                tmp = tryright(piece)
+    p,j,pit = (0,0,bytes([127]))  # p   - piece to drop
+                                  # j   - jet to consider
+                                  # pit - state of game
+    T[(p,j,pit)] = 0
+    H[(p,j,pit)] = 0
 
-            if not collision(pit,tmp,pos):
-                piece = tmp
+    t = 1
+    while True:
+        assert t <= duration
+        # state after t rocks
+        p,j,pit          = next_state(p,j,pit,jets)
 
-            if not collision(pit,piece,pos-1):
-                pos -= 1
-            else:
-                break
-        place(pit,piece,pos)
-        highest = max(highest,pos+len(piece)-1)
+        # detect the cycle
+        if (p,j,pit[-MEM:]) in Confs:
+            break
 
-    # printchamber(chamber)
-    print("part1:",highest)
+        Confs.add((p,j,pit[-MEM:]))
+        T[(p,j,pit[-MEM:])] = t
+        H[(p,j,pit[-MEM:])] = height(pit)
+        t += 1
+
+    # manage the cycle
+    cycled_state   = (p,j,pit[-MEM:])
+    first_height   = H[cycled_state]
+    second_height  = height(pit)
+    first_time     = T[cycled_state]
+    second_time    = t
+
+    # residual loops, blocks and tail
+    blocks = (duration - first_time) // (second_time-first_time)
+    tail   = (duration - first_time) %  (second_time-first_time)
+    for _ in range(tail):
+        p,j,pit = next_state(p,j,pit,jets)
+        assert (p,j,pit[-MEM:]) in Confs
+
+    return height(pit) + (second_height-first_height)*(blocks-1)
+
+def part1(data=None):
+    print("part1:",solve_naive(data,2022))
+
+def part2(data=None):
+    n=1_000_000_000_000
+    print("part2:",solve_cycle_detect(data,n))
 
 if __name__ == "__main__":
-    part1(2022,EXAMPLE)
-    part1(2022)
-    # part2()
+    part1(EXAMPLE)
+    part1()
+    part2(EXAMPLE)
+    part2()
