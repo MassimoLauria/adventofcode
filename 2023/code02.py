c4dee9266d8fc15a510d574b9742a5cb30b9645d
Massimo Lauria <massimo.lauria@uniroma1.it>
2023-12-02 10:37:48 +0100

    Revision

--- 2023/code02.py
+++ 2023/code02.py
@@ -18,34 +18,25 @@ def readdata(data=None):
     lines=data.splitlines()
     games=[]
     for line in lines:
-        if line.isspace() or len(line)==0: continue
-        line=line.replace(',',' ')
-        s=line.find(":")
-        segments=line[s+1:].split(';')
-        segments=[s.split() for s in segments]
-        games.append(segments)
+        line=line.split()
+        if len(line)<2: continue
+        gdata=[]
+        for i in range(2,len(line),2):
+            gdata.append((line[i+1][0],int(line[i])))
+        games.append(gdata)
     # clean
-    for game in games:
-        for j in range(len(game)):
-            round = game[j]
-            rgb=[0,0,0]
-            for i in range(0,len(round),2):
-                if round[i+1]=='red': rgb[0]=int(round[i])
-                if round[i+1]=='green': rgb[1]=int(round[i])
-                if round[i+1]=='blue': rgb[2]=int(round[i])
-            game[j] = rgb
     return games
 
 def part1(data=None):
     """solve part 1"""
-    mr,mg,mb=12,13,14
+    mrgb={'r':12,'g':13,'b':14}
     games=readdata(data)
     acc=0
+    good=True
     for gid,game in enumerate(games,start=1):
         good=True
-        for r,g,b in game:
-            if r>mr or g>mg or b>mb:
-                good=False
+        for c,v in game:
+            if v>mrgb[c]: good=False
         if good: acc+=gid
     print(acc)
 
@@ -54,14 +45,15 @@ def part2(data=None):
     games=readdata(data)
     s=0
     for gid,game in enumerate(games,start=1):
-        mr = max(r for r,_,_ in game)
-        mg = max(g for _,g,_ in game)
-        mb = max(b for _,_,b in game)
-        s +=mr*mg*mb
+        m={'r':0,'g':0,'b':0}
+        for c,v in game:
+            m[c]=max(v,m[c])
+        s +=m['r']*m['g']*m['b']
     print(s)
 
 
 if __name__ == "__main__":
+    print(readdata(EXAMPLE))
     part1(EXAMPLE)
     part1()
     part2(EXAMPLE)
