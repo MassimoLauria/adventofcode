faee826e8e3152448104ec3e438c1a22a5c58335
Massimo Lauria <massimo.lauria@uniroma1.it>
2023-12-06 15:18:48 +0100

    Parsing cleanup

--- 2023/code06.py
+++ 2023/code06.py
@@ -3,52 +3,27 @@
 import math
 
 
-EXAMPLE = """
-Time:      7  15   30
-Distance:  9  40  200
-"""
+EXAMPLE = """Time:      7  15   30
+             Distance:  9  40  200"""
 
-INPUT06 = """
-Time:        55     99     97     93
-Distance:   401   1485   2274   1405
-"""
-
-def readdata(data):
-    """Read and parse the input data"""
-    data=data.split()
-    races=[]
-    for i in range(1,len(data)//2):
-        races.append((int(data[i]),int(data[i+len(data)//2])))
-    return races
+INPUT06 = """Time:        55     99     97     93
+             Distance:   401   1485   2274   1405"""
 
 def ways_win(time,record):
-    Delta = time*time - 4*(record+1)
-    assert Delta>0
-    start = (time - math.sqrt(Delta)) / 2
-    start = int(math.ceil(start))
-    assert start*(time-start)>0
-    return time - 2*start + 1
+    delta = time*time - 4*(record+1)
+    lower = (time - math.sqrt(delta)) / 2
+    return time - 2*int(math.ceil(lower)) + 1
 
 def part1(data):
     """solve part 1"""
-    data=data.split()
-    races=[]
-    for i in range(1,len(data)//2):
-        races.append((int(data[i]),int(data[i+len(data)//2])))
-    acc=1
-    for time,record in races:
-        acc*=ways_win(time,record)
-    print(acc)
+    data=[x.split()[1:] for x in data.splitlines()]
+    races=[ (int(data[0][i]),int(data[1][i])) for i in range(len(data[0])) ]
+    print(math.prod(ways_win(t,r) for t,r in races ))
 
 def part2(data):
-    data=data.split()
-    time=[]
-    record=[]
-    for i in range(1,len(data)//2):
-        time.append(data[i])
-        record.append(data[i+len(data)//2])
-    time=int("".join(time))
-    record=int("".join(record))
+    data=[x.split()[1:] for x in data.splitlines()]
+    time   = int("".join(data[0]))
+    record = int("".join(data[1]))
     print(ways_win(time,record))
 
 
