c81417c08730dc7342f39475e93d3bcc056b6e73
Massimo Lauria <massimo.lauria@uniroma1.it>
2023-12-06 08:46:40 +0100

    AoC 2023 day 06

--- 2023/code06.py
+++ 2023/code06.py
@@ -1,5 +1,7 @@
 """Advent of Code 2023 day 06
 """
+import math
+
 
 EXAMPLE = """
 Time:      7  15   30
@@ -14,23 +16,45 @@ Distance:   401   1485   2274   1405
 def readdata(data):
     """Read and parse the input data"""
     data=data.split()
-    t=[]
-    d=[]
+    races=[]
     for i in range(1,len(data)//2):
-        t.append(int(data[i]))
-        d.append(int(data[i+len(data)//2]))
-    return t,d
+        races.append((int(data[i]),int(data[i+len(data)//2])))
+    return races
 
+def ways_win(time,record):
+    Delta = time*time - 4*record
+    assert Delta>0
+    start = (time - math.sqrt(Delta)) / 2
+    start = int(math.ceil(start))
+    if start*(time-start)==record: start+=1
+    assert start*(time-start)>0
+    return time - 2*start + 1
 
 def part1(data):
     """solve part 1"""
-    time,distance=data
-    print(time,distance)
+    data=data.split()
+    races=[]
+    for i in range(1,len(data)//2):
+        races.append((int(data[i]),int(data[i+len(data)//2])))
+    acc=1
+    for time,record in races:
+        acc*=ways_win(time,record)
+    print(acc)
+
+def part2(data):
+    data=data.split()
+    time=[]
+    record=[]
+    for i in range(1,len(data)//2):
+        time.append(data[i])
+        record.append(data[i+len(data)//2])
+    time=int("".join(time))
+    record=int("".join(record))
+    print(ways_win(time,record))
+
 
 if __name__ == "__main__":
-    EXAMPLE = readdata(EXAMPLE)
-    INPUT06 = readdata(INPUT06)
     part1(EXAMPLE)
     part1(INPUT06)
-    # part1()
-    # part2()
+    part2(EXAMPLE)
+    part2(INPUT06)
