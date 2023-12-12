100a8ed599e0c8311c053d04f6bd050a575e5f58
Massimo Lauria <massimo.lauria@uniroma1.it>
2023-12-12 17:51:13 +0100

    Use index for recutsion

--- 2023/code12.py
+++ 2023/code12.py
@@ -22,54 +22,50 @@ def readdata(data=None):
         patterns.append((pattern,tuple(lengths)))
     return patterns
 
-def howmany(D,pattern,lengths):
-    dots=0
-    while dots<len(pattern) and pattern[dots]=='.': dots+=1
-    pattern = pattern[dots:]
+def howmany(D,pattern,lengths,p,l):
+    while p<len(pattern) and pattern[p]=='.': p+=1
 
-    if (pattern,lengths) in D:
-        return D[pattern,lengths]
+    if (p,l) in D:
+        return D[p,l]
 
-    if len(lengths)==0:
-        return 0 if "#" in pattern else 1
+    if l==len(lengths):
+        return 0 if "#" in pattern[p:] else 1
 
-    if sum(lengths)+len(lengths)-1 > len(pattern):
+    if sum(lengths[l:])+len(lengths)-l-1 > len(pattern)-p:
         return 0
 
     start_with_dots=0
     start_with_block=0
-    if pattern[0]=='?':
-        start_with_dots = howmany(D,pattern[1:],lengths)
-    bsize=lengths[0]
-    block_compatible = bsize<=len(pattern) and \
-                       not ('.' in pattern[:bsize])
-    if len(pattern)>bsize and pattern[bsize]=='#':
+    if pattern[p]=='?':
+        start_with_dots = howmany(D,pattern,lengths,p+1,l)
+
+    dest =p+lengths[l]
+    block_compatible = dest<=len(pattern) and \
+                       not ('.' in pattern[p:dest])
+    if len(pattern)>dest and pattern[dest]=='#':
         block_compatible = False
     if block_compatible:
-        start_with_block = howmany(D,pattern[bsize+1:],lengths[1:])
+        start_with_block = howmany(D,pattern,lengths,dest+1,l+1)
 
-    D[pattern,lengths] = start_with_dots + start_with_block
-    return D[pattern,lengths]
+    D[p,l] = start_with_dots + start_with_block
+    return D[p,l]
 
 
 def part1(data=None):
     """solve part 1"""
-    D={}
     somma=0
-    for p,l in readdata(data):
-        somma+=howmany(D,p,l)
+    for pp,ll in readdata(data):
+        somma+=howmany({},pp,ll,0,0)
     print(somma)
 
 
 def part2(data=None):
-    """solve part 1"""
-    D={}
+    """solve part 2"""
     somma=0
     for p,l in readdata(data):
-        x=howmany(D,'?'.join([p]*5),l*5)
+        x=howmany({},'?'.join([p]*5),l*5,0,0)
         somma+=x
-        #print(x)
-    print(somma,len(D))
+    print(somma)
 
 
 if __name__ == "__main__":
