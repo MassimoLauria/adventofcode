397631d3ced05e470bd8f2cf4902c8ef87c9818c
Massimo Lauria <massimo.lauria@uniroma1.it>
2023-12-14 11:29:03 +0100

    AoC 2023 day 14

--- 2023/code14.py
+++ 2023/code14.py
@@ -16,6 +16,47 @@ O.#..O.#.#
 def printm(M):
     for l in M:
         print("".join(l))
+    print()
+
+def mhash(M):
+    return tuple([''.join(l) for l in M])
+
+def tilt(M,direction):
+    R,C = len(M),len(M[0])
+    if direction=='north':
+        outerrange=range(C)
+        innerrange=range(R)
+        delta=+1
+        coords=lambda o,i: (i,o)
+    elif direction=='south':
+        outerrange=range(C)
+        innerrange=range(R-1,-1,-1)
+        coords=lambda o,i: (i,o)
+        delta=-1
+    elif direction=='west':
+        outerrange=range(R)
+        innerrange=range(C)
+        coords=lambda o,i: (o,i)
+        delta=+1
+    elif direction=='east':
+        outerrange=range(R)
+        innerrange=range(C-1,-1,-1)
+        coords=lambda o,i: (o,i)
+        delta=-1
+    else:
+        return
+
+    for o in outerrange:
+        fallpoint=innerrange[0]
+        for i in innerrange:
+            r,c = coords(o,i)
+            if M[r][c]=='O':
+                fr,fc=coords(o,fallpoint)
+                M[r][c]='.'
+                M[fr][fc]='O'
+                fallpoint = fallpoint+delta
+            elif M[r][c]=='#':
+                fallpoint = i+delta
 
 def readdata(data=None):
     """Read and parse the input data"""
@@ -26,27 +67,55 @@ def readdata(data=None):
     M=[list(x) for x in data]
     return M
 
-
 def part1(data=None):
-    """solve part 1"""
     M=readdata(data)
+    tilt(M,'north')
+    print(totalload(M))
+
+def cycle(M):
+    tilt(M,'north')
+    tilt(M,'west')
+    tilt(M,'south')
+    tilt(M,'east')
+
+def totalload(M):
+    total=0
     R,C = len(M),len(M[0])
-    totalload=0
-    for j in range(C):
-        nextload=R
-        i=0
-        while i<R:
+    for i in range(R):
+        for j in range(C):
             if M[i][j]=='O':
-                totalload+=nextload
-                nextload-=1
-            elif M[i][j]=='#':
-                nextload=R-i-1
-            i+=1
-    print(totalload)
-    #printm(M)
+                total+=(R-i)
+    return total
+
+def part2(data=None):
+    """solve part 2"""
+    CONFs={}
+    M=readdata(data)
+    CONFs[mhash(M)]=0
+    for i in range(1,10**9+1):
+        cycle(M)
+        t=mhash(M)
+        if t in CONFs:
+            loopstart=CONFs[t]
+            loopend=i
+            break
+        else:
+            CONFs[t] = i
+    loopsize=loopend-loopstart
+    #print("Loop detected",loopstart,loopend)
+    # for x in range(100):
+    #     for i in range(loopsize):
+    #         cycle(M)
+    #     t2=mhash(M)
+    #     assert t==t2
+    cyclestodo=(10**9 - loopstart) % loopsize
+    for i in range(cyclestodo):
+        cycle(M)
+    print(totalload(M))
 
 
 if __name__ == "__main__":
     part1(EXAMPLE)
     part1()
-    # part2()
+    part2(EXAMPLE)
+    part2()
