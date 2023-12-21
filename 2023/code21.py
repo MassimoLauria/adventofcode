a6b058b955495e4f0a0b365b37e4113c20fb2d74
Massimo Lauria <massimo.lauria@uniroma1.it>
2023-12-21 11:38:32 +0100

    AoC 2023 day 21 part 1

--- 2023/code21.py
+++ 2023/code21.py
@@ -1,6 +1,9 @@
 """Advent of Code 2023 day 21
 """
 
+from collections import deque,defaultdict
+
+
 EXAMPLE = """...........
 .....###.#.
 .###.##..#.
@@ -24,19 +27,45 @@ def readdata(data=None):
     if data is None:
        with open("input21.txt") as f:
             data = f.read()
-    M = [list(x) for x in data.splitlines()]
-    for r in range(len(M)):
-        for c in range(len(M[0])):
-            if M[r][c]=='S':
-                return r,c,M
+    return [list(x) for x in data.splitlines()]
 
-def part1(data=None):
+def part1(max_dist,data=None):
     """solve part 1"""
-    sr,sc,M=readdata(data)
+    M=readdata(data)
     R,C=len(M),len(M[0])
+    sc = C // 2
+    sr = R // 2
+    Q=deque()
+    Q.append((sr,sc))
+    DIST={}
+    DIST[sr,sc]=0
+    while len(Q)>0:
+        r,c = Q.popleft()
+        if DIST[r,c]==max_dist:
+            continue
+        for dr,dc in [(+1,0),(-1,0),(0,1),(0,-1)]:
+            nr,nc=r+dr,c+dc
+            if nr<0 or nc<0 or nr>=R or nc>=C:
+                continue
+            if M[nr][nc]=='#' or (nr,nc) in DIST:
+                continue
+            DIST[nr,nc]=DIST[r,c]+1
+            Q.append((nr,nc))
+    even=0
+    for r,c in DIST:
+        if DIST[r,c]%2==0:
+            even+=1
+    print(even)
 
+def part2(data=None):
+    """solve part 2"""
+    N=26501365
+    M=readdata(data)
+    R,C=len(M),len(M[0])
+    sc = C // 2
+    sr = R // 2
 
 if __name__ == "__main__":
-    part1(EXAMPLE)
-    part1()
-    # part2()
+    part1(6,EXAMPLE)
+    part1(64)
+    part2(EXAMPLE)
