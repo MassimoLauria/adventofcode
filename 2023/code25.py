f366e06e733cbbde784f8cc1f3a5f95825ae9da8
Massimo Lauria <massimo.lauria@uniroma1.it>
2023-12-26 22:18:16 +0100

    AoC 2023 day 25 simplification

--- 2023/code25.py
+++ 2023/code25.py
@@ -1,11 +1,6 @@
 """Advent of Code 2023 day 25
 """
 
-import random
-import math
-import copy
-from collections import defaultdict
-
 EXAMPLE = """jqt: rhn xhk nvd
 rsh: frs pzl lsr
 xhk: hfx
@@ -22,8 +17,6 @@ frs: qnr lhk lsr
 """
 
 import networkx as nx
-from networkx.algorithms.flow.maxflow import minimum_cut
-
 
 def print_graph(G, filename):
     with open(filename, "w", encoding='utf-8') as f:
@@ -49,38 +42,11 @@ def readdata(data=None):
 
 def part1(data=None):
     G = readdata(data)
-    pairs = ((u, v) for u in G for v in G if u < v)
-    for u, v in pairs:
-        v, part = minimum_cut(G, u, v)
-        if v == 3.0:
-            print(len(part[0]) * len(part[1]))
-            break
-
-
-class uf():
-
-    def __init__(self, U):
-        self.parent = {x: x for x in U}
-        self.size = len(self.parent)
-
-    def part(self, k):
-        if self.parent[k] == k:
-            return k
-        self.parent[k] = self.part(self.parent[k])
-        return self.parent[k]
-
-    def join(self, u, v):
-        pu = self.part(u)
-        pv = self.part(v)
-        if pu != pv:
-            self.parent[pv] = pu
-            self.size -= 1
-
-    def same(self, u, v):
-        return self.parent[u] == self.parent[v]
-
-    def __len__(self):
-        return self.size
+    edges=nx.minimum_edge_cut(G)
+    G.remove_edges_from(edges)
+    c1,c2 = list(nx.connected_components(G))
+    assert len(edges)==3
+    print(len(c1)*len(c2))
 
 
 
@@ -88,7 +54,3 @@ class uf():
 if __name__ == "__main__":
     part1(EXAMPLE)
     part1()
-    # Ge = readdata(EXAMPLE)
-    # print_graph(Ge,'input25_example.dot')
-    G = readdata()
-    # print_graph(G,'input25_input.dot')
