f1b4463442d0f8c62430c93d53bf56caf2086f42
Massimo Lauria <massimo.lauria@uniroma1.it>
2024-12-29 12:22:30 +0100

    AoC 20219 day 20

--- 2019/code19.py
+++ 2019/code19.py
@@ -8,39 +8,60 @@ def part1():
     with open("input19.txt") as f:
         code = f.read()
     counter=0
-    for i in range (50):
-        for j in range(50):
+    for i in range (5):
+        for j in range(10):
             x=IntCode(code,[i,j])
             if x[0]==1:
-                print('#',end="")
                 counter+=1
-            else:
-                print('.',end="")
-        print()
+    a,b=0,1
+    for i in range (5,50):
+        a,b = beamcover(code, i, a, b-a)
+        if a> 49:
+            continue
+        counter += min(50,b) -a
+
     print(f"Part1: {counter}")
+
+def beamcover(code,x, ylb, difflb):
+    if x<5:
+        raise ValueError("No beam in this row")
+    s=ylb
+    # first start
+    while IntCode(code,[x,s])[0]!=1:
+        s+=1
+    diff=difflb
+    e = s+diff
+    # find upper bound to end (via doubling)
+    while IntCode(code,[x,e])[0]==1:
+        e+=diff
+        diff *=2
+    # find end (via binary search)
+    le=s
+    re=e
+    while re-le>1:
+        mid = (le+re) // 2
+        if IntCode(code,[x,mid])[0]==1:
+            le = mid
+        else:
+            re = mid
+    return s,re
 
 def part2():
     """solve part 1"""
     with open("input19.txt") as f:
         code = f.read()
-    counter=0
-    sj=0
-    ej=0
-    ends=[]
-    for i in range (4,1000):
-        while True:
-            x=IntCode(code,[i,sj])
-            if x[0]==1:
-                break
-            sj+=1
-        while True:
-            x=IntCode(code,[i,ej+1])
-            if x[0]==0:
-                break
-            sj+=1
-
-
-    print(f"Part1: {counter}")
+    N=100
+    alpha,beta = beamcover(code, N, 0, 1)
+    guessx = (N*100 + beta * 100) // (beta-alpha)
+    value=0
+    for x in range(guessx,guessx+20):
+        blow,bhigh = beamcover(code, x-99, alpha*(x-99)//N, (beta-alpha)*(x-99) //N )
+        alow,ahigh = beamcover(code, x, blow, bhigh-blow)
+        if bhigh-alow == 100:
+            value = (x-99)*10000 + alow
+            break
+        # check possible solution
+    print(f"Part2: {value}")
 
 
 
