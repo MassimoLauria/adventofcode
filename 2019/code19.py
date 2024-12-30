6f064ebc89393940f3a14e2ef6b4eda52eabe1a6
Massimo Lauria <massimo.lauria@uniroma1.it>
2024-12-30 12:45:02 +0100

    Edits and btw it was day 19 before, not 20

--- 2019/code19.py
+++ 2019/code19.py
@@ -15,53 +15,51 @@ def part1():
                 counter+=1
     a,b=0,1
     for i in range (5,50):
-        a,b = beamcover(code, i, a, b-a)
+        a,b = beamcover(code, i, a)
         if a> 49:
             continue
         counter += min(50,b) -a
 
     print(f"Part1: {counter}")
 
-def beamcover(code,x, ylb, difflb):
+def beamcover(code,x, ylb):
     if x<5:
         raise ValueError("No beam in this row")
-    s=ylb
+    alpha=ylb
     # first start
-    while IntCode(code,[x,s])[0]!=1:
-        s+=1
-    diff=difflb
-    e = s+diff
+    while IntCode(code,[x,alpha])[0]!=1:
+        alpha+=1
+    diff=1
+    beta = alpha+1
     # find upper bound to end (via doubling)
-    while IntCode(code,[x,e])[0]==1:
-        e+=diff
+    while IntCode(code,[x,beta])[0]==1:
+        beta += diff
         diff *=2
     # find end (via binary search)
-    le=s
-    re=e
-    while re-le>1:
-        mid = (le+re) // 2
+    lowbeta=alpha
+    while beta-lowbeta>1:
+        mid = (beta+lowbeta) // 2
         if IntCode(code,[x,mid])[0]==1:
-            le = mid
+            lowbeta = mid
         else:
-            re = mid
-    return s,re
+            beta = mid
+    return alpha,beta
 
 def part2():
     """solve part 1"""
     with open("input19.txt") as f:
         code = f.read()
     N=100
-    alpha,beta = beamcover(code, N, 0, 1)
-    guessx = (N*100 + beta * 100) // (beta-alpha)
-    value=0
-    for x in range(guessx,guessx+20):
-        blow,bhigh = beamcover(code, x-99, alpha*(x-99)//N, (beta-alpha)*(x-99) //N )
-        alow,ahigh = beamcover(code, x, blow, bhigh-blow)
+    alpha,beta = beamcover(code, N, 0)
+    x = (N*100 + beta * 100) // (beta-alpha)
+    while True:
+        blow,bhigh = beamcover(code, x-99, alpha*(x-99)//N )
+        alow,_ = beamcover(code, x, blow)
         if bhigh-alow == 100:
-            value = (x-99)*10000 + alow
+
             break
-        # check possible solution
-    print(f"Part2: {value}")
+        x+=1
+    print(f"Part2: {(x-99)*10000 + alow}")
 
 
 
