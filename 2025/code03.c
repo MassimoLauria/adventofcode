b3939a9fad837259c711459abd6c1e09efc6c672
Massimo Lauria <massimo.lauria@uniroma1.it>
2025-12-03 14:15:07 +0100

    Part 1

---
+++ 2025/code03.c
@@ -0,0 +1,156 @@
+// Advent of Code 2025 day XX
+
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <time.h>
+
+char example[]="987654321111111\n811111111111119\n234234234234278\n818181911112111\n";
+
+struct AllocatedArray {
+    void *end;
+    ssize_t datasize;
+    ssize_t len;
+};
+
+struct AllocatedArray* GetArray(void * array) {
+    return ((struct AllocatedArray*)(array - sizeof(struct AllocatedArray)));
+}
+
+
+ssize_t Len(void * array) {
+    return ((struct AllocatedArray*)(array - sizeof(struct AllocatedArray)))->len;
+}
+
+ssize_t Capacity(void * array) {
+    struct AllocatedArray *x = (struct AllocatedArray*)(array - sizeof(struct AllocatedArray));
+    return  (x->end-array)/x->datasize;
+}
+
+void Extend(void *array,ssize_t n) {
+    struct AllocatedArray *x = (struct AllocatedArray*)(array - sizeof(struct AllocatedArray));
+    if (x->end < array+(n+x->len)*x->datasize) {
+        perror("AllocArray: Extension failure. No more capacity");
+        exit(EXIT_FAILURE);
+    }
+    x->len += n;
+}
+
+int mod(int x,int d){
+    return (d + (x % d)) % d;
+}
+
+/* Allocation of an array with length and capacity
+
+   |END-POINT|DATASIZE|IIIIIIII|.... data ....|
+
+*/
+void* AllocArray(ssize_t  len, ssize_t capacity, ssize_t objsize) {
+    if (len==0 || objsize==0 || len>capacity) {
+        perror("AllocArray: Invalid parameters");
+        exit(EXIT_FAILURE);
+    }
+    void *mem = malloc(sizeof(struct AllocatedArray)+ objsize*capacity);
+    if (mem==NULL) {
+        perror("AllocArray failed");
+        exit(EXIT_FAILURE);
+    }
+    struct AllocatedArray* pack=mem;
+    pack->end=mem + sizeof(struct AllocatedArray)+ objsize*capacity;
+    pack->len=len;
+    pack->datasize=objsize;
+    return mem+sizeof(struct AllocatedArray);
+}
+
+void *parse_text(ssize_t textlen, char *p) {
+    return p;
+}
+
+
+char* load_file(char *filename) {
+    struct stat st;
+    if (stat(filename,&st)==-1) {
+        perror(filename);
+        exit(EXIT_FAILURE);
+    }
+    ssize_t n=st.st_size;
+    char *buffer = AllocArray(n, n,sizeof(char));
+    FILE *f = fopen(filename,"r");
+    if (f==NULL) {
+        perror(filename);
+        exit(EXIT_FAILURE);
+    }
+    if (fread(buffer,1,n,f)!=n) {
+        perror(" ha letto tutti i bytes");
+        exit(EXIT_FAILURE);
+    }
+    return buffer;
+}
+
+
+int part1(ssize_t textlen, char *p) {
+    int64_t best_prefix;
+    int64_t best;
+    int64_t candidate;
+    char *textend=p+textlen;
+    int64_t jolt=0;
+    while(p<textend && *p!='\0') {
+        best_prefix=p[0]-'0';
+        best=0;
+        candidate=0;
+        p=p+1;
+        while(*p!='\n') {
+            candidate=*p -'0' + 10*best_prefix;
+            if (candidate > best) {
+                best = candidate;
+            }
+            if ((*p -'0') > best_prefix) {
+                best_prefix = (*p-'0');
+            }
+            p++;
+        }
+        p++;
+        jolt+=best;
+    }
+    return jolt;
+}
+
+int part2(ssize_t textlen, char *p) {
+    parse_text(textlen,p);
+    return 42;
+}
+
+int main() {
+    clock_t start,end;
+    int res;
+    char *buffer;
+
+    start=clock();
+	buffer = load_file("input03.txt");
+	end = clock();
+    printf("Loading data                                  - %f\n", ((double)(end-start))/CLOCKS_PER_SEC);
+
+    start=clock();
+	res = part1(sizeof(example),example);
+	end = clock();
+    printf("Part1 - example   : %-25d - %f\n", res, ((double)(end-start))/CLOCKS_PER_SEC);
+
+    start=clock();
+    res = part1(Len(buffer), buffer );
+	end = clock();
+    printf("Part1 - challenge : %-25d - %f\n", res, ((double)(end-start))/CLOCKS_PER_SEC);
+
+    start=clock();
+	res = part2(sizeof(example),example);
+	end = clock();
+    printf("Part2 - example   : %-25d - %f\n", res, ((double)(end-start))/CLOCKS_PER_SEC);
+
+    start=clock();
+    res = part2(Len(buffer), buffer);
+	end = clock();
+    printf("Part2 - challenge : %-25d - %f\n", res, ((double)(end-start))/CLOCKS_PER_SEC);
+
+    return 0;
+}
